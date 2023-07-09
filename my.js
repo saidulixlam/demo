7c72ebc36f12a82ed071c791ddbc73072ce4926c
saidulixlam <saidulixlam@gmail.com>
2023-07-10 00:34:28 +0530

    adding new

---
+++ my.js
@@ -0,0 +1,5 @@
+let titl= document.getElementById('main-header');
+titl.style.border='solid 3px black';
+var add=document.querySelector('.title-a');
+add.style.fontWeight='bold';
+add.style.color='green';
